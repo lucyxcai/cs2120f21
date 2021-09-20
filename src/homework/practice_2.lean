ebd93bf70a88217a51f7daff0d7a61a22bb088fc
Lucy Cai <gew6rm@virginia.edu>
2021-09-20 20:18:24 +0000

    hw 2

--- src/homework/practice_2.lean
+++ src/homework/practice_2.lean
@@ -37,46 +37,122 @@ example : ∀ (P : Prop), P ∧ P ↔ P :=
 begin
   assume P, 
 	apply iff.intro _ _,
-	assume porp, 
-	apply and.elim porp, 
+	assume pandp, 
+	apply and.elim pandp, 
 	assume p, 
-  assume p,
+  assume p, 
   exact p,
 	--backwards 
 	assume p, 
-	exact and.intro P p,
+	exact and.intro p p,
 end
 
 example : ∀ (P Q : Prop), P ∨ Q ↔ Q ∨ P := 
 begin
   assume P,
-  apply iff.into _ _,
-  assume porp,
+  assume Q,
+  apply iff.intro _ _,
+  assume porq,
+  apply or.elim porq,
+  assume pq, 
+  exact pq,
+  assume pq,
+  exact pq,
+  assume pq,
+  exact or.intro_left P pq,
+  exact or.intro_right Q pq,
 
 end
 
 example : ∀ (P Q : Prop), P ∧ Q ↔ Q ∧ P := 
 begin
+  assume P, 
+  assume Q, 
+	apply iff.intro _ _,
+	assume pandp, 
+	apply and.elim pandp, 
+	assume pq,
+  exact pq,
+  assume pq,
+  exact pq,
+	--backwards 
+	assume pq, 
+	exact and.intro pq pq,
 end
 
 example : ∀ (P Q R : Prop), P ∧ (Q ∨ R) ↔ (P ∧ Q) ∨ (P ∧ R) := 
 begin
+  assume P,
+  assume Q,
+  assume R, 
+  apply iff.intro _ _,
+  assume pandp,
+  apply and.elim pandp,
+  assume porp,
+  apply or.elim porp,
+  assume pqr,
+  exact pqr, 
+  assume pqr,
+  exact pqr,
+  assume pqr,
+  exact and.intro pqr pqr,
 end
 
 example : ∀ (P Q R : Prop), P ∨ (Q ∧ R) ↔ (P ∨ Q) ∧ (P ∨ R) := 
 begin
+  assume P,
+  assume Q,
+  assume R, 
+  apply iff.intro _ _,
+  assume pandp,
+  apply and.elim pandp,
+  assume porp,
+  apply or.elim porp,
+  assume pqr,
+  exact pqr, 
+  assume pqr,
+  exact pqr,
+  assume pqr,
+  exact or.intro_left PQR pqr,
 end
 
 example : ∀ (P Q : Prop), P ∧ (P ∨ Q) ↔ P := 
 begin
+  assume P,
+  assume Q,
+  apply iff.intro _ _,
+  assume pandp,
+  apply and.elim pandp,
+  assume porp,
+  apply or.elim porp,
+  assume pq,
+  exact pq, 
+  assume pq,
+  exact pq,
+  assume pq,
+  exact and.intro pq pq,
 end
 
 example : ∀ (P Q : Prop), P ∨ (P ∧ Q) ↔ P := 
 begin
+  assume P,
+  assume Q,
+  apply iff.intro _ _,
+  assume pandp,
+  apply and.elim pandp,
+  assume porp,
+  apply or.elim porp,
+  assume pq,
+  exact pq, 
+  assume pq,
+  exact pq,
+  assume pq,
+  exact or.intro_left PQ pq,
 end
 
 example : ∀ (P : Prop), P ∨ true ↔ true := 
 begin
+  
 end
 
 example : ∀ (P : Prop), P ∨ false ↔ P := 
