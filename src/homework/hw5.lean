c738d1ee9b8d219e83428256b2bdc2d5cb4d6705
Lucy Cai <gew6rm@virginia.edu>
2021-10-25 00:11:49 +0000

    hw5

--- src/homework/hw5.lean
+++ src/homework/hw5.lean
@@ -30,7 +30,9 @@ In this context complete the following tasks:
 
 (1) Write a fluent English-language statement
 of the proposition to be proved. 
-
+There exists a function f that takes a value of type α and returns a value of type β. For all 
+values a of type α in set p, the function f is applied. Thus, we get a set q consisting of 
+type β values derived from set p. 
 (2) Provide a formal proof of the proposition.
 
 (3) Write an informal proof of the proposition.
@@ -47,18 +49,22 @@ What does this propositon say? Explain it in
 plain English. Here's a start: "If there's a 
 function that maps/takes every α value that ... 
 -- your completed English rendition here:
+If there's a function that takes every α value that is given and returns a type β value, 
+then this proposition takes every α value in set p and returns a set q consisting of β values
+after applying the function to each value in set p.  
 -/
 
 
 -- Give your formal proof here
 begin
-  assume n, 
-  assume h, 
-  cases n with w pf,
-  cases h with r pr, 
   
-  
-  
+  assume h1,
+cases h1 with f e,
+assume h2,
+have e2 := exists_imp_exists e h2,
+cases e2 with a qfa,
+have b := f a,
+apply exists.intro b,
 end
   
 
